87f67f548f93bbc52b21d4f94d321c59b0ef68c8
Alexey Emelyanov <alex3791hunter@gmail.com>
2021-09-22 00:01:28 +0300

    added special sort

--- main.cpp
+++ main.cpp
@@ -1,21 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-void out(){
-    string str;
+void out(int q){
+    string str,name,surname,book;
     fstream outf;
+    set<vector<string>> s;
     outf.open("res.txt",ios::in);
-    set<string> s;
-    int i=0;
-    int cnt=0;
-    while(getline(outf,str)){
-        if(i%2==1){
-            s.insert(str);
-            cnt++;
+    if(q==1){
+        while(outf>>name>>surname>>book){
+            vector<string> a(3);
+            a[0]=name;
+            a[1]=surname;
+            a[2]=book;
+            s.insert(a);
         }
-        i++;
+        for(auto elem: s){
+            cout<<elem[0]<<" "<<elem[1]<<" "<<elem[2]<<endl;
+        }
+    }else if(q==2){
+        while(outf>>name>>surname>>book){
+            vector<string> a(3);
+            a[0]=surname;
+            a[1]=name;
+            a[2]=book;
+            s.insert(a);
+        }
+        for(auto elem: s){
+            cout<<elem[1]<<" "<<elem[0]<<" "<<elem[2]<<endl;
+        }
+    }else if(q==3){
+        while(outf>>name>>surname>>book){
+            vector<string> a(3);
+            a[0]=book;
+            a[1]=name;
+            a[2]=surname;
+            s.insert(a);
+        }
+        for(auto elem: s){
+            cout<<elem[1]<<" "<<elem[2]<<" "<<elem[0]<<endl;
+        }
     }
-    cout<<cnt<<" people "<<s.size()<<" different books";
 }
+
 signed main(){
     string cmd;
     cout<<"Enter 1 if you want to answer the questions of the survey or enter 2 if you want to see the results of the survey"<<endl;
@@ -29,14 +54,14 @@ signed main(){
     getline(cin,surname);
     fstream outf;
     outf.open("res.txt",ios::app);
-    outf<<name<<" "<<surname<<endl;
     string str;
     cout<<"What book do you recommend to read?"<<endl;
     getline(cin,str);
-    outf<<str<<" ";
-    outf<<endl;
+    outf<<name<<" "<<surname<<" "<<str<<endl;
     }else if(cmd=="2"){
-        out();
+        int q;
+        cin>>q;
+        out(q);
     }
     return 0;
 }
